fe19817882aa81398d12f55f42f51372edeb0875
Monroe Clinton <contact@monroeclinton.com>
2023-08-25 22:39:44 -0400

    Service::Ext<Request> documentation

--- src/main.rs
+++ src/main.rs
@@ -175,6 +175,7 @@ async fn main() -> anyhow::Result<()> {
 
     // Create a service from a series of layers/service
     let svc = ServiceBuilder::new()
+        // Maximum of two connections at a time
         .layer(ConcurrencyLimitLayer::new(2))
         .layer_fn(|service| Logger::new(service, "layer_fn".to_string()))
         .layer(LoggerLayer)
@@ -190,6 +191,9 @@ async fn main() -> anyhow::Result<()> {
         // Create a Logger<Logger<Responder>> service
         let mut svc = factory_svc.call(()).await?;
 
+        // The ConcurrencyLimit service blocks until there is available capacity, so call
+        // ServiceExt::<Request>::ready to block until ready.
+        // https://docs.rs/tower/0.4.13/tower/trait.ServiceExt.html#method.ready
         tokio::spawn(ServiceExt::<TcpStream>::ready(&mut svc).await?.call(stream));
     }
 }
